090db5541178144e0cfb4f41e5014d3bb2b5efb1
Tombro-93 <79336436+Tombro-93@users.noreply.github.com>
2021-07-29 14:57:42 +0100

    test three

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route,} from 'react-router-dom';
 import Home from './pages/index';
 import SigninPage from './pages/login';
 import ShopPage from './pages/store';
